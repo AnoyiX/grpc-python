824ae8311e239c1428b191492bcdcae4a091dc9b
anoy <545544032@qq.com>
2019-08-23 11:35:16 +0800

    Fixed pipy description

---
+++ python-grpc/README.md
@@ -0,0 +1,7 @@
+python-grpc An Common Service RPC library
+------
+Based on `grpcio`，we define the common grpc service and function. By reflection & syntactic sugar，server side can provide rpc service easily, also client can declare rpc interface easily.
+
+Project HomePage
+------
+[https://github.com/ChinaSilence/python-grpc](https://github.com/ChinaSilence/python-grpc)
--- python-grpc/setup.py
+++ python-grpc/setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 setup(
     name='python-grpc',
-    version='0.0.4',
+    version='0.0.5',
     description='common grpc service',
     author='Anoyi',
     author_email='545544032@qq.com',
     url='https://github.com/ChinaSilence/python-grpc',
     py_modules=['grpclib', 'service_pb2', 'service_pb2_grpc'],
-    install_requires=['grpcio>=1.23.0','protobuf>=3.9.1']
+    install_requires=['grpcio>=1.23.0', 'protobuf>=3.9.1'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
